9911133ece477408b38009c30fc1ba5af4f567b4
Mick Sexton <lacrookedbeat@hotmail.com>
2017-06-09 22:25:50 -0700

    add html hookup to js, some styling on css, and tidy'd up js code and inserted some comments for clarification

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,66 @@
+$(document).ready(function() {
+	$("#formOne").submit(function() {
+		event.preventDefault();
+		var newString = $("input#inputPhrase").val();
+		//EXAMPLE: "don't compare yourself to others, compare yourself to the person you were yesterday";
+		newString = newString.replace(/[ !%$;.,_'-?]/g,'');
+		newString = newString.toLowerCase();
 
-var inputString = "don't compare yourself to others, compare yourself to the person you were yesterday";
-var newString = "";
-newString = inputString.replace(/[ !%$;.,_'-?]/g,'');
-newString = newString.toLowerCase();
+		var totalElements = newString.length;
+		var sqRoot = Math.sqrt(totalElements);
+		var columns;
+		var rows;
 
-var totalElements = newString.length;
-var sqRoot = Math.sqrt(totalElements);
-var columns;
-var rows;
+		if (sqRoot%1 === 0) {
+			columns = sqRoot;
+		  rows = sqRoot;
+		} else {
+			columns = Math.floor(sqRoot);
+		  rows = (Math.floor(sqRoot)) + 1;
+		}
 
-if (sqRoot%1 === 0) {
-	columns = sqRoot;
-  rows = sqRoot;
-} else {
-	columns = Math.floor(sqRoot);
-  rows = (Math.floor(sqRoot)) + 1;
-}
+		var normalArray = [];
+		var encryptArray = [];
+		var letterCount = 0;
 
-var normalArray = [];
-var encryptArray = [];
-var letterCount = 0;
+		for (var i=0; i < rows; i++) {
+			var newArray = [];
+		  normalArray[i] = newArray;
+		  //normalArray[i] = new Array(rows);
 
-for (var i=0; i < rows; i++) {
-	var newArray = [];
-  normalArray[i] = newArray;
-  //normalArray[i] = new Array(rows);
+		  for (var j=0; j < columns; j++) {
+		    normalArray[i][j] = newString.charAt(letterCount);
+		    letterCount++;
+		  }
+		}
 
-  for (var j=0; j < columns; j++) {
-    normalArray[i][j] = newString.charAt(letterCount);
-    letterCount++;
-  }
-}
+		var elementCount = 0;
 
-var elementCount = 0;
+		for(var i=0; i < columns; i++) {
+			for(var j=0; j < rows; j++) {
+		  	encryptArray[elementCount] = normalArray[j][i];
+		    elementCount++;
+		  }
+		}
 
-for(var i=0; i < columns; i++) {
-	for(var j=0; j < rows; j++) {
-  	encryptArray[elementCount] = normalArray[j][i];
-    elementCount++;
-  }
-}
+		//clear out any empty arrays
+		for (var i=0; i < encryptArray.length; i++) {
+			if (encryptArray[i] === "" ) {
+		  	encryptArray.splice(i,1);
+		  }
+		}
 
-//clear out any empty arrays
-for (var i=0; i < encryptArray.length; i++) {
-	if (encryptArray[i] === "" ) {
-  	encryptArray.splice(i,1);
-  }
-}
+		//add spaces in every fifth element
+		for (var i=0; i < encryptArray.length; i++) {
+			if ((i+1)%5===0) {
+		  	encryptArray[i] = encryptArray[i] + " ";
+		  }
+		}
 
-for (var i=0; i < encryptArray.length; i++) {
-	if ((i+1)%5===0) {
-  	encryptArray[i] = encryptArray[i] + " ";
-  }
-}
-
-var outputString = encryptArray.toString();
-outputString = outputString.replace(/[,]/g,'');
-alert(outputString);
+		var outputString = encryptArray.toString();
+		outputString = outputString.replace(/[,]/g,'');
+		//alert(outputString);
+		$("#outputPhrase").text(outputString);
+		$("#result").show();
+	});
+});
